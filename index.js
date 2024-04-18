f625b12e68334ea72de49b5be14f475c07dfb955
KinozzzFilms <121505558+KinozzzFilms@users.noreply.github.com>
2024-04-18 23:06:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+if(location.search.length == 0){
+  var url = 'https://t.me/slivmens_sklad';
+}else{
+  var url = 'http://slivmens-courses.ru/'+location.search;
+  var element = document.getElementById("redirect")
+  element.innerHTML =  url;
+  element.setAttribute("href", url)
+}
+window.location.href('http://slivmens-courses.ru/?url=dGcx');
+var sec=30;
+ function Sec(url)
+ {
+  document.getElementById("sec").innerHTML=sec;
+   sec--;
+   if(sec==1)
+   {
+   	  window.location.href(url)
+   }
+   setTimeout('Sec()',1000);
+ }
+ 
+ //Sec(url);
+console.log(url)
